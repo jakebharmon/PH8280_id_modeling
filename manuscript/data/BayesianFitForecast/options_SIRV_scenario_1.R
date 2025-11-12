bca3145f032ef439add750749eee14124646aa39
jakebharmon <jakebluesharmon@gmail.com>
2025-11-12 16:51:10 -0500

    Updated equation to remove extraneous parameters and have cumulative case counts for fitting

--- manuscript/data/BayesianFitForecast/options_SIRV_scenario_1.R
+++ manuscript/data/BayesianFitForecast/options_SIRV_scenario_1.R
@@ -1,8 +1,8 @@
 # ----------------------------
 # Calibration / forecasting metadata
 # ----------------------------
-calibrationperiods <- c(17)
-forecastinghorizon <- 10
+calibrationperiods <- c(51)
+forecastinghorizon <- 0
 
 model_name <- "scenario_one_half_vaccination"
 cadfilename1 <- "influenza_df"
@@ -14,94 +14,70 @@ datetype <- "Weeks"
 # ----------------------------
 # State variables
 # ----------------------------
-vars <- c("S","I1","I2","R1","R2","V")
+vars <- c("S","I1","I2","R1","R2","V","C1","C2")
 
 # ----------------------------
 # Parameter vector and ordering
 # ----------------------------
-# "Lambda",  # params1   birth/input rate
-# "beta1",   # params2   transmission rate strain 1
-# "beta2",   # params3   transmission rate strain 2
-# "mu",      # params4   natural death rate
-# "gamma1",  # params5   recovery rate from I1 -> R1
-# "gamma2",  # params6   recovery rate from I2 -> R2
-# "nu"       # params7   vaccination rate (S -> V)
-# "N"        # params8   population
-# "i0"       # params9   initial infected
-# "rho"      # params10  reporting fraction
-# "sigma12"  # params11  Crossimmunity to 2 given 1
-# "sigma21"  # params12  Crossimmunity to 1 given 2
-params <- c("Lambda","beta1","beta2",
-            "mu","gamma1","gamma2",
-            "nu","N","i0","rho",
-            "sigma12","sigma21")
+# "beta1",   # params1   transmission rate strain 1
+# "beta2",   # params2   transmission rate strain 2
+# "gamma1",  # params3   recovery rate from I1 -> R1
+# "gamma2",  # params4   recovery rate from I2 -> R2
+# "nu",      # params5   vaccination rate (S -> V)
+# "N",       # params6   population size
+# "rho"      # params7   reporting fraction
 
-paramsfix <- c(1,0,0,
-               1,0,0,
-               1,1,1,0,
-               1,1)
+params <- c("beta1","beta2",
+            "gamma1","gamma2",
+            "nu","N","rho")
+
+paramsfix <- c(0,0,
+               0,0,
+               1,1,0)
 
 # ----------------------------
 # Priors and initial conditions 
 # Lower / upper bounds for all parameters in two-strain SIRV model
 # ----------------------------------------------------------
 
-# 1: Lambda (input rate)
-# Fix to 0 for constant population / short epidemic window
-params1_prior  <- 0 
-
-# 2–3: beta1, beta2 (transmission rates)
-params2_prior <- "normal(2, 0.2)T[0,]"
+# 1-2: beta1, beta2 (transmission rates)
+params1_prior <- "normal(1.8, 0.2)T[0,]"
+params1_LB <- 0
+params1_UB <- 3
+params2_prior <- "normal(1.5, 0.2)T[0,]"
 params2_LB <- 0
-params2_UB <- 10
-params3_prior <- "normal(1.8, 0.2)T[0,]"
-params3_LB <- 0
-params3_UB <- 10
+params2_UB <- 3
 
-# 4: mu (natural death rate)
-# Human life expectancy ~70 years 
-params4_prior <- 2.7e-4
-
-# 5–6: gamma1, gamma2 (recovery rates)
+# 3-4: gamma1, gamma2 (recovery rates)
 # Infectious period 3–5 days
-params5_prior <- "normal(1.5, 0.2)T[0,]"
-params5_LB <- 0
-params5_UB <- 5
-params6_prior <- "normal(1.5, 0.2)T[0,]"
-params6_LB <- 0
-params6_UB <- 5
+params3_prior <- "normal(1, 0.1)T[0,]"
+params3_LB <- 0
+params3_UB <- 2
+params4_prior <- "normal(1, 0.1)T[0,]"
+params4_LB <- 0
+params4_UB <- 2
 
-# 7: nu (vaccination rate)
+# 5: nu (vaccination rate)
 # annual coverage c ≈ 40% or 60%
 # 40 %	0.010
 # 50 %	0.013
 # 60 %	0.019
-params7_prior <- 0.013
+params5_prior <- 0.013
 
-# 8: N (population)
-params8_prior <- 1e5
+# 6: N (population)
+params6_prior <- 3e6
 
-# 9: i0 (initial infected)
-params9_prior <- 15
-
-# 10: rho (reporting fraction)
-# mean ~ 0.333;underreporting
-params10_prior <- "beta(2, 4)"
-params10_LB <- 0
-params10_UB <- 1
-
-# 11-12: sigma (cross immunity)
-params11_prior <- .95
-params12_prior <- .95
-
+# 7: Reporting fraction
+params7_prior <- "beta(2,4)"
+params7_LB <- 0
+params7_UB <- 1
 
 # ----------------------------
 # Observation / error model priors
 # ----------------------------
 errstrc <- 1
-normalerror1_prior <- "cauchy(0, 2.5)" 
-negbinerror1_prior <- "exponential(5)"
-negbinerror2_prior <- "exponential(5)"
+negbinerror1_prior <- "exponential(1)"
+negbinerror2_prior <- "exponential(1)"
 
 # ----------------------------
 # Time dependent templates
@@ -113,15 +89,14 @@ time_dependent_templates <- list()
 # S (vars1), I1 (vars2), I2 (vars3), R1 (vars4), R2 (vars5), V (vars6)
 # ----------------------------
 ode_system <- '
-  diff_var1 = params1 - (params2 * vars1 * vars2 / params8) - (params3 * vars1 * vars3 / params8) - params7 * vars1 - params4 * vars1
-  diff_var2 = (params2 * vars1 * vars2 / params8) + ((1 - params12) * params2 * vars5 * vars2 / params8) - (params4 + params5) * vars2
-  diff_var3 = (params3 * vars1 * vars3 / params8) + ((1 - params11) * params3 * vars4 * vars3 / params8) - (params4 + params6) * vars3
-  diff_var4 = params5 * vars2 - (1 - params11) * (params3 * vars4 * vars3 / params8) - params4 * vars4
-  diff_var5 = params6 * vars3 - (1 - params12) * (params2 * vars5 * vars2 / params8) - params4 * vars5
-  diff_var6 = params7 * vars1 - params4 * vars6
-  y1 = params10 * vars2 
-  y2 = params10 * vars3
-  y3 = vars6
+  diff_var1 = - (params1 * vars1 * vars2 / params6) - (params2 * vars1 * vars3 / params6) - params5 * vars1
+  diff_var2 = (params1 * vars1 * vars2 / params6) - params3 * vars2
+  diff_var3 = (params2 * vars1 * vars3 / params6) - params4 * vars3
+  diff_var4 = params3 * vars2
+  diff_var5 = params4 * vars3
+  diff_var6 = params5 * vars1
+  diff_var7 = params7 * (params1 * vars1 * vars2 / params6)
+  diff_var8 = params7 * (params2 * vars1 * vars3 / params6)
 '
 
 
@@ -136,18 +111,18 @@ composite_expressions <- list(
 # ----------------------------
 # Data / file / disease metadata
 # ----------------------------
-fitting_index <- c(2,3)
+fitting_index <- c(7,8)
 fitting_diff <- c(1,1)
 
 # ----------------------------
 # Initial conditions 
 # S (vars1), I1 (vars2), I2 (vars3), R1 (vars4), R2 (vars5), V (vars6)
 # ----------------------------
-Ic <- c(1e5 - 15, 12, 3, 0, 0, 0)
+Ic <- c(3e6 - 500, 300, 200, 0, 0, 0, 0, 0)
 vars.init <- 1
 
 # ----------------------------
 # MCMC / sampling settings
 # ----------------------------
 niter <- 1000
-num_chain <- 4
+num_chain <- 2
